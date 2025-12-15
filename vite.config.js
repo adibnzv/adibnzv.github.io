3cf12fdc6d1e30c18016c648b7266434211af78d
bzman101 <40146242+bzman101@users.noreply.github.com>
2025-12-15 18:34:25 +0200

    fix try

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',          // user site must be root
-  build: { outDir: 'docs' } // keep build output separate from source
+  build: { outDir: 'docs' }, // <-- important
+  base: '/',                 // custom domain -> keep '/'
 })
